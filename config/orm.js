ba1665f7c76b9b925c09e8dccfbe210ebd035171
Keita Suzuki <ksuzuki417@gmail.com>
2020-10-18 00:09:51 -0500

    ORM function created

---
+++ config/orm.js
@@ -0,0 +1,30 @@
+const connection = require("../config/connection.js");
+
+const orm = {
+//Select All ORM function
+    selectAll: function(tableName) {
+        const queryString = "SELECT * FROM ??";
+        connection.query(queryString, [tableName], function(err, res) {
+            if(err) throw err;
+            console.log(res);
+        });
+    },
+//Insert One ORM function
+    insertOne: function(tableName, whatToAdd, boolean, val1, val2) {
+        const queryString = "INSERT INTO ?? (??, ??) VALUE ?, ?";
+        console.log(queryString);
+        connection.query(queryString, [tableName, whatToAdd, boolean, val1, val2], function(err, res) {
+            if(err) throw err;
+            console.log(res);
+        });
+    },
+//Update One ORM function
+    updateOne: function(tableName, whatToUpdate, val1, whereToUpdate, val2) {
+        const queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+        console.log(queryString);
+        connection.query(queryString, [tableName, whatToUpdate, val1, whereToUpdate, val2], function(err, res) {
+            if(err) throw err;
+            console.log(res);
+        });
+    }
+}
