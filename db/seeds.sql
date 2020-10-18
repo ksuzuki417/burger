fdf8abf4afe03f6d674a672fc20bfd97b50dc24b
Keita Suzuki <ksuzuki417@gmail.com>
2020-10-17 23:31:31 -0500

    files, database created. npm package installed

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheese Burger", 0);
