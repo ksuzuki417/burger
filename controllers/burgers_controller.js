0e0876492179a76325f2ed0df026c49f12a0365e
Keita Suzuki <ksuzuki417@gmail.com>
2020-10-20 23:15:36 -0500

    router created

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,8 +3,22 @@ const router = express.Router();
 
 const burger = require("../models/burger.js");
 
+//create the router for the app
 router.get("/", function(req, res) {
     burger.selectAll(function(data) {
-        
-    })
-})
+        var hbsObject = {
+            burgers_db: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+        res.json({id: result.insertId });
+    });
+});
+
+//Export the router
+module.exports = router;
